9e893afdc51cc07b81cfa96b8505e4f7d5ad2ff9
Andrew <abadia962@gmail.com>
2017-10-07 08:15:20 -0500

    API's de tweeter y SQLite3

--- Clase-4/App/init.py
+++ Clase-4/App/init.py
@@ -6,92 +6,76 @@ db.echo = True #esto evita que la salida de los datos sea por consola
 
 metadata = MetaData(db)
 
-# usuarios = Table('usuarios', metadata, autoload = True)
-# correos = Table('correos', metadata, autoload = True)
-#
-# sesion = create_session()
-#
-# class User(object):
-#     def __init__(self, nombre=None, edad=None, passw=None):
-#         self.nombre = nombre
-#         self.edad = edad
-#         self.passw = passw
-#
-#     def __repr__(self):
-#         return self.nombre
-#
-# class Correo(object):
-#     def __init__(self,dirreccion=None):
-#         self.dirrecion = dirreccion
-#     def __repr__(self):
-#         return self.dirrecion
-#
-# correomapper = mapper(correos, Correo)
-# usuariomapper = mapper(Usuario)
+usuarios = Table('usuarios', metadata, autoload = True)
+correos = Table('correos', metadata, autoload = True)
 
-# usuarios = Table('usuarios', metadata, autoload = True)
-# correos = Table('correos', metadata, autoload = True)
-#
-# sesion = create_session()
-#
-# class User(object):
-#     def __init__(self, nombre=None, edad=None, passw=None):
-#         self.nombre = nombre
-#         self.edad = edad
-#         self.passw = passw
-#
-#     def __repr__(self):
-#         return self.nombre
-#
-# class Correo(object):
-#     def __init__(self,dirreccion=None):
-#         self.dirrecion = dirreccion
-#     def __repr__(self):
-#         return self.dirrecion
-#
-# correomapper = mapper(correos, Correo)
-# usuariomapper = mapper(Usuario)
+sesion = create_session()
 
+class User(object):
+    def __init__(self, nombre=None, edad=None, passw=None):
+        self.nombre = nombre
+        self.edad = edad
+        self.passw = passw
 
+    def __repr__(self):
+        return self.nombre
+
+class Correo(object):
+    def __init__(self,dirreccion=None):
+        self.dirrecion = dirreccion
+    def __repr__(self):
+        return self.dirrecion
+
+correomapper = mapper(correos, Correo)
+usuariomapper = mapper(User, usuarios, properties = {
+    'correo' : relation(correomapper),
+})
+
+usuarios = sesion.query(User).all()
+correos = sesion.query(Correo).Count()
+
+for usuario in usuarios:
+    print(usuario)
+print('Tenemos ' + )
 
 
 #-----------------------------------------------------------------------------------------#
 
-usuarios = Table('usuarios', metadata,
-                 Column('usuario_id', Integer, primary_key=True),
-                 Column('nombre', String(30)),
-                 Column('edad', Integer),)
-usuarios.create()
-
-correos = Table('correos', metadata,
-                Column('correo_id', Integer, primary_key=True),
-                Column('dirreccion', String),
-                Column('usuario_id', Integer, ForeignKey('usuarios.usuario_id')),)
-correos.create()
-
-i = usuarios.insert()
-i.execute(
-    {'nombre': 'Petra', 'edad': 50},
-    {'nombre': 'Juana', 'edad': 45},
-    {'nombre': 'Xenobia', 'edad': 17},
-    {'nombre': 'Tinoca', 'edad': 25},
-)
-
-i = correos.insert()
-i.execute(
-    {'direccion': 'petra@.com', 'usuario_id': 1},
-    {'direccion': 'juana@nidea.com', 'usuario_id': 2},
-    {'direccion': 'juana@ejemplo.com', 'usuario_id': 2},
-    {'direccion': 'xndb@.com', 'usuario_id': 3},
-)
-
-def ejecutar(stnc):
-    rs = stnc.execute()
-    for fila in rs:
-        print(fila)
-
-s = select([usuarios, correos], correos.c.usuario_id == usuarios.c.usuario_id)
-ejecutar(s)
+# usuarios = Table('usuarios', metadata,
+#                  Column('usuario_id', Integer, primary_key=True),
+#                  Column('nombre', String(30)),
+#                  Column('edad', Integer),)
+# usuarios.create()
+#
+# correos = Table('correos', metadata,
+#                 Column('correo_id', Integer, primary_key=True),
+#                 Column('dirreccion', String),
+#                 Column('usuario_id', Integer, ForeignKey('usuarios.usuario_id')),)
+# correos.create()
+#
+# i = usuarios.insert()
+# i.execute(
+#     {'nombre': 'Petra', 'edad': 50},
+#     {'nombre': 'Juana', 'edad': 45},
+#     {'nombre': 'Xenobia', 'edad': 17},
+#     {'nombre': 'Tinoca', 'edad': 25},
+# )
+#
+# i = correos.insert()
+# i.execute(
+#     {'direccion': 'petra@.com', 'usuario_id': 1},
+#     {'direccion': 'juana@nidea.com', 'usuario_id': 2},
+#     {'direccion': 'juana@ejemplo.com', 'usuario_id': 2},
+#     {'direccion': 'xndb@.com', 'usuario_id': 3},
+# )
+#
+# def ejecutar(stnc):
+#     rs = stnc.execute()
+#     for fila in rs:
+#         print(fila)
+#
+# s = select([usuarios, correos], correos.c.usuario_id == usuarios.c.usuario_id)
+# ejecutar(s)
 
 # s = select([usuarios.c.nombre, correos.c.dirrecion], correos.c.usuario_id == usuarios.c.usuario_id)
 # ejecutar(s)
